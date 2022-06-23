025e8b14c37d54eccb9bf894f21d5098de07f4c7
Albert Sanahuja <albert.sanahuja@ctfc.cat>
2022-06-23 14:45:24 +0200

    0.0 learning C++

---
+++ test/main.cpp
@@ -0,0 +1,22 @@
+/* ************************************
+ * author: ASL
+ * task: Learning C++ 
+ * ************************************/
+
+
+#include <iostream>
+//int main(int argc, char **argv)
+int main()
+{
+//	printf("hello world\n");
+	
+	int fnumber;
+	
+	std::cout << "Number? ";
+	
+	std::cin >> fnumber;
+	
+	std::cout << "Yess, " << fnumber << " of course!" << std::endl;
+	
+	return 0;
+}
